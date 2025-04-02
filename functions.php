2eeaa1542d0a113658863080f6da8e4ac28c156c
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-04-02 20:30:38 +0200

    CP GIT

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ require_once get_template_directory() . '/includes/models/cpt-footer.php'; // Cu
 require_once get_template_directory() . '/includes/models/cpt-404.php'; // Custom Post Type 404
 
 // MISE A JOUR
-if ($pagenow === 'update-core.php') require_once get_template_directory() . '/ocade-updater.php';
+if (is_admin()) require_once get_template_directory() . '/ocade-updater.php';
 
 // SUPPORTS
 add_action('after_setup_theme', function () {
